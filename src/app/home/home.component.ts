83cf49dc608b826e9500555350d6ce50f0e51a70
Will <doggy.huang@gmail.com>
2023-07-24 11:33:32 +0800

    示範 Angular 15 之前元件是如何取得路由參數的

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  route = inject(ActivatedRoute);
 
+  id: string | null = '';
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+  }
 }
